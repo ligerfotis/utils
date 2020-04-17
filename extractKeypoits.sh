e30927881898dafd96f9d1ca2c7fa053ba77de55
Lygerakis Fotios <ligerfotis@gmail.com>
2020-04-16 21:12:09 -0500

    Add files via upload

--- extractKeypoits.sh
+++ extractKeypoits.sh
@@ -6,42 +6,44 @@ participant=$1
 
 echo "Participant number: ${participant}"
 
+for i in {1..3}
+	do
+	BASE_DIR="/home/ligerfotis/CSE6963_rosbag_data/Experiment2"
+	SUB_DIR="Participant_${participant}$i"
+	current_dir="${BASE_DIR}/${SUB_DIR}"
 
-BASE_DIR="/home/ligerfotis/CSE6963_rosbag_data/Experiment2"
-SUB_DIR="Participant_${participant}"
-current_dir="${BASE_DIR}/${SUB_DIR}"
+	topics=("/camera/color/camera_info" "/camera/color/image_raw" "/camera/depth_registered/points" "/tf")
+	topics1=("/keypoint_3d_matching" "/tf")
+	topcis2=("/topic_transform")
 
-topics=("/camera/color/camera_info" "/camera/color/image_raw" "/camera/depth_registered/points" "/tf")
-topics1=("/keypoint_3d_matching" "/tf")
-topcis2=("/topic_transform")
-
-if [ ! -d ${current_dir} ] 
-then
-	echo "Dir ${current_dir} not found"
-    exit 1
-fi
-
-GESTURES=("cube" "polygon" "cup" "botle" "plate")
-
-for ((j=0; j< ${#GESTURES[@]}; j++ ))
-do	
-	DIRE="${current_dir}/${GESTURES[j]}"
-
-	if [ ! -d ${DIRE} ] 
-	then 
-		echo "Dir ${DIRE} not found"
-    exit 1
+	if [ ! -d ${current_dir} ] 
+	then
+		echo "Dir ${current_dir} not found"
+	    exit 1
 	fi
-	
-	file="${GESTURES[j]}_${participant}.bag"
 
-	./CSE6963_rosbag_data/openTerminals.sh "${DIRE}/${file}" "${DIRE}/kp_${file}"
+	GESTURES=("cube" "polygon" "cup" "botle" "plate")
+	# GESTURES=("cube" )
 
-	
+	for ((j=0; j< ${#GESTURES[@]}; j++ ))
+	do	
+		DIRE="${current_dir}/${GESTURES[j]}"
+
+		if [ ! -d ${DIRE} ] 
+		then 
+			echo "Dir ${DIRE} not found"
+	    exit 1
+		fi
+		
+		file="${GESTURES[j]}_${participant}.bag"
+
+		./CSE6963_rosbag_data/openTerminals.sh "${DIRE}/${file}" "${DIRE}/kp_${file}"
+
+		
+	done
+
+	echo "${BASE_DIR} for Participant ${participant} is over"
 done
-
-echo "${BASE_DIR} for Participant ${participant} is over"
-
 
 
 
--- openTerminals.sh
+++ openTerminals.sh
@@ -3,16 +3,31 @@
 play_file=$1
 record_file=$2
 
-xterm -hold -e "roslaunch openpose_utils_launch whole_pipeline_realsense_with_marker.launch" &
-sleep 3
-xterm -hold -e "roslaunch keypoint_3d_matching keypoint_3d_matching.launch" &
-sleep 3
-echo "Playing ${play_file} on demand"
-roslaunch bag_read_service bag_read_service.launch bag_file:="${play_file}"
+xterm -geometry 96x24+2000+0 -hold -e "roslaunch openpose_utils_launch whole_pipeline_realsense_with_marker.launch" &
+pid1="$!"
+sleep 2
 
-rosservice call /next_msg 
+xterm -geometry 96x24-0+0 -hold -e "roslaunch keypoint_3d_matching keypoint_3d_matching.launch" &
+pid2="$!"
+sleep 2
+
 echo "Recording ${record_file} on demand"
+xterm -geometry 96x24+2000-0 -hold -e "rosbag record /keypoint_3d_matching /tf -O "${record_file}" __name:=my_bag " &
+pid4="$!"
 
-# xterm -hold -e "rosbag play file2.bag"
-killall xterm
+echo "Playing ${play_file} on demand"
+xterm -geometry 96x24-0-0 -hold -e "sleep 3;rosservice call /next_msg"  &
+pid3="$!"
+
+
+roslaunch bag_read_service bag_read_service.launch bag_file:="${play_file}"
+rosnode kill /my_bag
+sleep 2
+rosbag reindex "${record_file}"
+
+kill "$pid1"
+kill "$pid2"
+kill "$pid3"
+kill "$pid4"
+
 exit 0
